5d3c47a4d4d529b31cef0f9676fd4233fc3a4389
JKrup <graziano.laveder@gmail.com>
2020-08-20 14:56:43 +0200

    Updates

---
+++ precache-manifest.f1b48a9ad8c87961d9f863eaa8241686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b16a3229163a705e3ebbaef972938d",
+    "url": "/monster-rolodex-react/index.html"
+  },
+  {
+    "revision": "54d24088b1177dbc51f5",
+    "url": "/monster-rolodex-react/static/css/main.0051f0bf.chunk.css"
+  },
+  {
+    "revision": "30c9e554bd9b1e03dc63",
+    "url": "/monster-rolodex-react/static/js/2.e7cba093.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monster-rolodex-react/static/js/2.e7cba093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d24088b1177dbc51f5",
+    "url": "/monster-rolodex-react/static/js/main.8515ed66.chunk.js"
+  },
+  {
+    "revision": "e4dd72f320065ee1140c",
+    "url": "/monster-rolodex-react/static/js/runtime-main.50c95c02.js"
+  }
+]);
